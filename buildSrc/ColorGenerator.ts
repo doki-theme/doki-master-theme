41ead543979befab4842a30bc4696bd41b41d39d
Alex Simons <alexsimons9999@gmail.com>
2022-07-14 18:41:45 -0500

    Updated Zero Two's & Hiro's background stuff.

--- buildSrc/ColorGenerator.ts
+++ buildSrc/ColorGenerator.ts
@@ -37,7 +37,7 @@ function blendColors(
 
 function addNewColor(dokiTheme: { dokiThemeDefinition: MasterDokiThemeDefinition; dokiFileDefinitionPath: string }) {
   // if(!dokiTheme.dokiThemeDefinition.dark) return;
-  if(dokiTheme.dokiThemeDefinition.id !== "13adffd9-acbe-47af-8101-fa71269a4c5c") return;
+  if(dokiTheme.dokiThemeDefinition.id !== "ceb8c11f-f736-420b-962b-fc0db26b3708") return;
 
   const headerColor = hex_to_rgba(
     dokiTheme.dokiThemeDefinition.colors.headerColor
